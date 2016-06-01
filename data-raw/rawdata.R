952500b822ead0c559f0db9bb438d9108bf45be5
Dario Masante <dariomasante@users.noreply.github.com>
2016-06-01 21:40:56 +0100

    Update rawdata.R

--- data-raw/rawdata.R
+++ data-raw/rawdata.R
@@ -6,21 +6,23 @@
 library(raster)
 library(bnspatial)
 
-rawPath = system.file("extdata", "LegalStatus.tif", package = "bnspatial")
-status = raster(rawPath)
+rawPath <- system.file("extdata/LegalStatus.tif", package = "bnspatial")
+status <- raster(rawPath)
+status <- readAll(status)
 
-rawPath = system.file("extdata", "degSlope.tif", package = "bnspatial")
-slope = raster(rawPath)
-slope[slope == 128] = NA
+rawPath <- system.file("extdata/degSlope.tif", package = "bnspatial")
+slope <- raster(rawPath)
+slope[slope == 128] <- NA
 
-rawPath = system.file("extdata", "CurrentLULC.tif", package = "bnspatial")
-currentLU = raster(rawPath)
+rawPath <- system.file("extdata/CurrentLULC.tif", package = "bnspatial")
+currentLU <- raster(rawPath)
+currentLU <- readAll(currentLU)
 
-rawPath = system.file("extdata", "LUclasses.txt", package = "bnspatial")
-LUclasses = importClasses(rawPath)
+rawPath <- system.file("extdata/LUclasses.txt", package = "bnspatial")
+LUclasses <- importClasses(rawPath)
 
-rawPath = system.file("extdata", "LandUseChange.net", package = "bnspatial")
-LandUseChange = loadNetwork(rawPath,'FinalLULC')
+rawPath <- system.file("extdata/LandUseChange.net", package = "bnspatial")
+LandUseChange <- loadNetwork(rawPath,'FinalLULC')
 
 spDataLst <- linkMultiple(c(currentLU, slope, status), LandUseChange, LUclasses, verbose = FALSE)
 coord <- aoi(currentLU, xy=TRUE)

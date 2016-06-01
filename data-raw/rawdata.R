ede5191737499b93d4893135f0baa6fc813f437c
Dario Masante <dariomasante@users.noreply.github.com>
2016-06-01 20:19:15 +0100

    Create rawdata.R

---
+++ data-raw/rawdata.R
@@ -0,0 +1,32 @@
+## Build the .RData from raw
+
+## Start an empty blank (vanilla) session
+
+## Land use change example
+library(raster)
+library(bnspatial)
+
+rawPath = system.file("extdata", "LegalStatus.tif", package = "bnspatial")
+status = raster(rawPath)
+
+rawPath = system.file("extdata", "degSlope.tif", package = "bnspatial")
+slope = raster(rawPath)
+slope[slope == 128] = NA
+
+rawPath = system.file("extdata", "CurrentLULC.tif", package = "bnspatial")
+currentLU = raster(rawPath)
+
+rawPath = system.file("extdata", "LUclasses.txt", package = "bnspatial")
+LUclasses = importClasses(rawPath)
+
+rawPath = system.file("extdata", "LandUseChange.net", package = "bnspatial")
+LandUseChange = loadNetwork(rawPath,'FinalLULC')
+
+spDataLst <- linkMultiple(c(currentLU, slope, status), LandUseChange, LUclasses, verbose = FALSE)
+coord <- aoi(currentLU, xy=TRUE)
+evidence <- bulkDiscretize(spDataLst, coord)
+
+rm(rawPath)
+rm(coord)
+rm(spDataLst)
+rm(.Random.seed)
